604c556a643d34bdd59d92450e41019358a132a7
Christian Gierl <c.gierl@outlook.com>
2023-03-19 20:06:02 +0100

    mudule 3 first try

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import { param } from 'jquery';
+import { fetchWithTimeout } from './services.js';
+
+const movie = require('./src/data/movies.json');
+
 export function fetchMovies() {
-    
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
+};
+
+const resolveCallback = (param) => {
+    console.log('success')
 }
 
+const moviePromise = fetchMovies();
+
+moviePromise.then(resolveCallback);
