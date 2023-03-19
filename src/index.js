2352f5e90c87a41802fd2eaa2a43a6517ace7d30
Christian Gierl <c.gierl@outlook.com>
2023-03-19 20:14:12 +0100

    module 3 - try 3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import { param } from 'jquery';
 import { fetchWithTimeout } from './services';
-
 const movies = require('./src/data/movies.json');
-
 export function fetchMovies() {
     const resolveFunction = () => movies;
     return fetchWithTimeout(1000).then(resolveFunction);
 };
 
+const moviePromise = fetchMovies();
 moviePromise.then(results => {
     console.log(results);
 });
-
-const resolveCallback = (param) => {
-    console.log(param)
-}
