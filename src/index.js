4f42a5903809a29bf0ee353e8836829891f0ecea
Christian Gierl <c.gierl@outlook.com>
2023-03-22 21:41:29 +0100

    bekannt

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { fetchWithTimeout } from "./services";
+import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
 const movies = require("./data/movies.json");
 
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies() {
+    return Promise.all([fetchMovies(), fetchBooks()])
+    .then(([books, movies ]) => ({
+        books,
+        movies
+    }))
+    .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-  console.log(results);
-});
+const getBooksAndMoviesPromise = getBooksAndMovies();
+
+getBooksAndMoviesPromise.then();
