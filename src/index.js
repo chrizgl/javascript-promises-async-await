32467a7fd4d27143e7e266e583e15d4e3b4aee9c
Christian Gierl <c.gierl@outlook.com>
2023-03-24 22:33:02 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -15,3 +15,17 @@ const getBooksAndMoviesPromise = getBooksAndMovies();
 getBooksAndMoviesPromise.then(results => {
     console.log('getBooksAndMoviesPromise', results);
 });
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+};
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+
+getBooksAndMoviesPromise.then(results => {
+    console.log('getBooksOrMoviesPromise', results);
+});
+
+getBooksOrMovies();
