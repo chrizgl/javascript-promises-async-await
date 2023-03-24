ce1ce33759b0d2394ab9032bfd9302c0d32bb481
Christian Gierl <c.gierl@outlook.com>
2023-03-24 22:48:49 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
 const movies = require("./data/movies.json");
 
-function getBooksAndMovies() {
-    return Promise.all([fetchMovies(), fetchBooks()])
-    .then(([books, movies ]) => ({
-        books,
-        movies
+const getBooksAndMovies = () => {
+  return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies]) => ({
+      books,
+      movies
     }))
     .catch(error => console.log("Error fetching books and movies", error));
-}
-
+};
 const getBooksAndMoviesPromise = getBooksAndMovies();
-
 getBooksAndMoviesPromise.then(results => {
-    console.log('getBooksAndMoviesPromise', results);
+  console.log("getBooksAndMoviesPromise", results);
 });
 
 function getBooksOrMovies() {
-    return Promise.race([fetchBooks(), fetchMovies()])
+  return Promise.race([fetchBooks(), fetchMovies()])
     .then(results => results)
-    .catch(error => console.log("Error waiting for the promise race", error));
+    .catch(error => {
+      console.log("Error waiting for the promise race", error);
+    });
 }
 
 const getBooksOrMoviesPromise = getBooksOrMovies();
-
 getBooksOrMoviesPromise.then(results => {
-    console.log('getBooksOrMoviesPromise', results);
+  console.log("getBooksOrMoviesPromise", results);
 });
